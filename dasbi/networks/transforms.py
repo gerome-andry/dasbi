e52cc33f116ac80c84d41c13d02487f02b47f691
Andry Gerome <andry.gerome@gmail.com>
2023-03-26 23:02:43 +0200

    Fix InvConv batch process

--- dasbi/networks/transforms.py
+++ dasbi/networks/transforms.py
@@ -102,11 +102,11 @@ class InvConv(Transform):
         batch_size = x.shape[0]
         z = torch.zeros_like(x)
 
-        for b in range(batch_size):
-            weights = self.conv_kern(self.net(self.kernel, context[b].unsqueeze(0)))
-            weights = weights.reshape((1, 1) + tuple(self.ks))
-            x_p = self.triang_pad(x[b].unsqueeze(0))
-            z[b] = nn.functional.conv2d(x_p, weights)
+        weights = self.conv_kern(self.net(self.kernel, context))
+        weights = weights.reshape((-1, 1) + tuple(self.ks))
+        x_p = self.triang_pad(x)
+        z = nn.functional.conv2d(x_p, weights)
+        z = z[:,:1,...]
 
         ladj = z.new_zeros(batch_size)
 
@@ -116,43 +116,39 @@ class InvConv(Transform):
         batch_size, c, h, w = z.shape
         x = torch.zeros_like(z)
 
-        for b in range(batch_size):
-            weights = self.conv_kern(self.net(self.kernel, context[b].unsqueeze(0)))
-            z_b = z[b].unsqueeze(0)
-            c_mat = self.fc_from_conv(weights, z_b)
-            z_b = z_b.permute((2, 3, 1, 0))
-            z_b = z_b.reshape((c * h * w, 1))
-            x[b] = (
-                torch.linalg.solve(c_mat, z_b).reshape(h, w, c, 1).permute((3, 2, 0, 1))
-            )
+        weights = self.conv_kern(self.net(self.kernel, context))
+        c_mat = self.fc_from_conv(weights, z)
+        x = z.permute((0, 2, 3, 1))
+        x = x.reshape((batch_size, c * h * w))
+        x = torch.linalg.solve(c_mat, x).reshape(batch_size, h, w, c).permute((0, 3, 1, 2))
 
         ladj = None
 
         return x, ladj
 
     def conv_kern(self, w):
-        ck = w.new_ones(tuple(self.ks))
-        ck[self.mask] = w
+        ck = w.new_ones((w.shape[0],)+tuple(self.ks))
+        ck[:,self.mask] = w
 
         return ck
 
     def fc_from_conv(self, kern, x):
-        xdim = x.shape[-2:]
+        xdim = (x.shape[0],) + x.shape[-2:]
         K = x.new_ones(xdim)
         K = self.triang_pad(K)
-        c_mat = x.new_zeros((x[0, ...].numel(),) * 2)
+        c_mat = x.new_zeros((x.shape[0],) + (x[0,0, ...].numel(),) * 2)
 
         row = 0
-        for i in range(K.shape[0] - kern.shape[0] + 1):
-            for j in range(K.shape[1] - kern.shape[1] + 1):
+        for i in range(K.shape[1] - kern.shape[1] + 1):
+            for j in range(K.shape[2] - kern.shape[2] + 1):
                 K_check = torch.zeros_like(K)
 
-                K_check[i : i + kern.shape[0], j : j + kern.shape[1]] = (
-                    kern * K[i : i + kern.shape[0], j : j + kern.shape[1]]
+                K_check[...,i : i + kern.shape[1], j : j + kern.shape[2]] = (
+                    kern * K[...,i : i + kern.shape[1], j : j + kern.shape[2]]
                 )
 
                 K_check = self.triang_unpad(K_check)
-                c_mat[row, :] = K_check.flatten()
+                c_mat[...,row, :] = K_check.flatten(start_dim =1)
                 row += 1
 
         return c_mat
@@ -310,4 +306,33 @@ if __name__ == "__main__":
     # print(z)
     # x, _ = ss.inverse(z)
     # print(x)
+    class ConvEmb(nn.Module):
+        def __init__(self, input_dim, output_lg):
+            super().__init__()
+            ks = torch.clamp(input_dim[-2:] // 3, 1)
+            strides = torch.clamp((input_dim[-2:] - ks)//7, 1)
+            self.conv1 = nn.Conv2d(input_dim[1], input_dim[1] * 4, tuple(ks), stride = tuple(strides))
+            self.apool_in = nn.AvgPool2d(tuple(ks), stride=tuple(strides))
+            self.conv2 = nn.Conv2d(input_dim[1] * 5, 1, (1, 1))
+            self.act = nn.ReLU()
+
+            self.lin = nn.Linear(torch.prod((input_dim[-2:] - ks)//strides + 1), output_lg)
+
+        def forward(self, x, y):
+            emb_y = self.conv1(y)
+            emb_y = self.act(emb_y)
+            emb_y = torch.cat((self.apool_in(y), emb_y), dim=1)
+            emb_y = self.conv2(emb_y)
+            emb_y = self.act(emb_y).flatten(start_dim = 1)
+            out = self.lin(emb_y) + x
+
+            return self.act(out)
+    torch.manual_seed(42)
+    ic = InvConv(torch.tensor((3,3)), ConvEmb(torch.tensor((1,1,1,1)), 8))
+    x = torch.randint(10, (2,1,3,3)).float()
+    print(x)
+    z,_ = ic(x, torch.ones((2,1,1,1)))
+    print(z)
+    x = ic.inverse(z, torch.ones((2,1,1,1)))
+    print(x)
     pass
