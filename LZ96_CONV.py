1b6148cb21469c941d9c4f6c21bced4efc2ec23b
Andry Gerome <andry.gerome@gmail.com>
2023-03-27 23:15:01 +0200

    Launch assim test

--- LZ96_CONV.py
+++ LZ96_CONV.py
@@ -32,7 +32,7 @@ PATH.mkdir(parents=True, exist_ok=True)
 N_grid = [2**i for i in range(3,10)]
 Y_grid = [int(np.ceil(x/4)) for x in N_grid]
 lN = len(N_grid)
-window = 1
+window = 10
 CONFIG = {
     # Architecture
     "embedding": [3]*lN,
@@ -125,7 +125,7 @@ def train(i: int):
     with open(config["observer_fp"], "rb") as handle:
         observer = pickle.load(handle)
 
-    run = wandb.init(project="dasbi", config=config, group="LZ96_scaling_step")
+    run = wandb.init(project="dasbi", config=config, group="LZ96_scaling_assim")
     runpath = PATH / f"runs/{run.name}_{run.id}"
     runpath.mkdir(parents=True, exist_ok=True)
 
