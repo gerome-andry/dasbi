fe7e6c7329ef1b077fb866def7df0901909654e4
Andry Gerome <andry.gerome@gmail.com>
2023-04-19 16:35:53 +0200

    fix run

--- retrieve_runs.py
+++ retrieve_runs.py
@@ -1,7 +1,24 @@
 from pathlib import Path
 import os
+from tqdm import tqdm
 
 SCRATCH = os.environ.get("SCRATCH", ".")
-runpath = Path(SCRATCH) / "npe_conv/lz96/run"
+runpath = Path(SCRATCH) / "npe_nsf/lz96/runs"
+destpath = Path(SCRATCH) / "res_maf_lz"
+#destpath.mkdir(parents = True, exist_ok = True)
+
 fd = filter(os.path.isdir, [os.path.join(runpath,f) for f in os.listdir(runpath)])
-print(fd)
+fd = list(fd)
+for d in tqdm(fd):
+    files = [os.path.join(d,f) for f in os.listdir(d)]
+    if len(files) == 0:
+        continue
+    f = files[-1]
+    with open(f, "rb") as src:
+        data = src.read()
+    dir_file = f.split("/")[-2:]
+    dp = Path(destpath) / dir_file[0]
+    dp.mkdir(parents = True, exist_ok = True)
+    
+    with open(Path(dp) / dir_file[1], "wb") as dest:
+        dest.write(data)
