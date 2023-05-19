9af3ffcd06e01205e3a6bb1a0792c12171e75af3
Andry Gerome <andry.gerome@gmail.com>
2023-05-19 18:04:27 +0200

    score lin

--- LZLIN_AUC.py
+++ LZLIN_AUC.py
@@ -57,7 +57,7 @@ CONFIG = {
     "embedding": [3]*lN,
     "depth": [dp[k] for k in N_grid],
     "input_h": [chan[k] for k in N_grid],
-    "N_ms": ["score"]*lN,
+    "N_ms": ["score_lin"]*lN,
     # Training
     "epochs": [512]*lN,
     "batch_size": [128]*lN,
